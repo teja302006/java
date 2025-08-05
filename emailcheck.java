a0d734901f2d108423cbefd593447a9a777e3aff
teja M <123854192+teja302006@users.noreply.github.com>
2025-08-05 22:08:47 +0530

    emailcheck.java

---
+++ emailcheck.java
@@ -0,0 +1,16 @@
+public class revers{
+      public static void main(String[] args){
+              String word="yoyoy@gmail.com";
+              if(word.contains("@gmail.com")){
+                          System.out.println("it is gmail id ");
+              }
+        else{
+          System.out.println("not an email id ");
+        }
+      }
+}
+
+
+
+
+
